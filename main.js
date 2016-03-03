3c460e99359560dffee6abdc68151a16ca46f7ee
Davide Brunetti <davide.brunetti@novetica.org>
2016-03-03 17:56:17 +0000

    Added clearInterval when click on .stage

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 chrome.extension.onConnect.addListener(function(port) {
 	
+	var milliseconds = 3000;
+	var interval = 'init';
+	console.log(interval);
+	
 	console.log("Connected .....");
 	port.onMessage.addListener(function(msg) {
 		
 		if(msg == "Start browsing"){
 			console.log("message recieved: " + msg);
-			var milliseconds = 3000;
-			setInterval(function sendRequest(){
+			port.postMessage("Browse it like there's no tomorrow!!!");
+			interval = setInterval(function sendRequest(){
 
 				port.postMessage("Browse it like there's no tomorrow!!!");
 
 			}, milliseconds);
+			console.log(interval);
 		}
 		else if(msg == "Stop browsing"){
 			console.log("message recieved: " + msg);
+			clearInterval(interval);
+			console.log(interval);
 		}
 		else{
 			console.log("There's something wrong in the message received from lib.js");	
